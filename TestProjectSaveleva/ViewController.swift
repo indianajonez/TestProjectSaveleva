b2e67fae74db246b5d265d10c2ebed63314647ba
indiana jonez <indiana.jonez@yandex.ru>
2024-01-23 11:17:05 +0300

    Initial Commit

---
+++ TestProjectSaveleva/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProjectSaveleva
+//
+//  Created by Ekaterina Saveleva on 23.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
